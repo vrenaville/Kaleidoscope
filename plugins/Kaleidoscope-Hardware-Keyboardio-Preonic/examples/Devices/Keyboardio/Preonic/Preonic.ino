f7293733b1a9768c713fbfe406646174398185d2
vrenaville <vincent.renaville@camptocamp.com>
2025-09-12 09:29:31 +0200

    fix: instance definitions values

--- plugins/Kaleidoscope-Hardware-Keyboardio-Preonic/examples/Devices/Keyboardio/Preonic/Preonic.ino
+++ plugins/Kaleidoscope-Hardware-Keyboardio-Preonic/examples/Devices/Keyboardio/Preonic/Preonic.ino
@@ -602,11 +602,14 @@ void setup() {
 //     // kaleidoscope::plugin::Qukey(0, KeyAddr(5, 9), Key_PageUp)
 // )
 // add space cadet mappings, left and right parens on alt keys
+
   static kaleidoscope::plugin::SpaceCadet::KeyBinding spacecadetmap[] = {
-     {Key_RightAlt, Key_LeftParen, 250}
-    , {Key_LeftAlt, Key_RightParen, 250}
-    , {Key_LeftControl, Key_RightBracket, 250}
-    , {Key_LeftShift, Key_LeftBracket, 250}
+     {Key_LeftAlt, Key_LeftParen, 250}
+    , {Key_LeftGui, Key_RightParen, 250}
+    , {Key_LeftControl, Key_LeftBracket, 250}
+    , {Key_RightAlt, Key_RightBracket, 250}
+    //, {ShiftToLayer(FUN), LSHIFT(Key_LeftBracket), 250}
+    //, {ShiftToLayer(RAISE), LSHIFT(Key_RightBracket), 250}
     , SPACECADET_MAP_END
   };
   SpaceCadet.setMap(spacecadetmap);
